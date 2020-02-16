f97e2740d6c7dd669ab53b3c4a7f11286014eab1
yeshua19pro <yeshuaangelito@gmail.com>
2020-02-16 17:26:53 -0500

    Creación de variables para la ecuacion parametrica de segmento de recta

--- ComputerGraphics/src/computergraphics/EcParSegReg.java
+++ ComputerGraphics/src/computergraphics/EcParSegReg.java
@@ -1,14 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package computergraphics;
 
 /**
  *
- * @author Yashua
+ * @author mmarulandc & ynarvaezp
  */
 public class EcParSegReg {
-    
+
+    int x1;
+    int x2;
+    int y1;
+    int y2;
+
+    int u1;
+    int u2;
+
+    EcParSegReg(int x1, int x2, int y1, int y2) {
+        this.x1 = x1;
+        this.y2 = x1;
+
+        this.x2 = x1;
+        this.y2 = x1;
+    }
+
+    public int solve(String epsr1, String epsr2) {
+        
+        
+        return 0;
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
+    }
+
+    public int getU1() {
+        return u1;
+    }
+
+    public void setU1(int u1) {
+        this.u1 = u1;
+    }
+
+    public int getU2() {
+        return u2;
+    }
+
+    public void setU2(int u2) {
+        this.u2 = u2;
+    }
+
 }
